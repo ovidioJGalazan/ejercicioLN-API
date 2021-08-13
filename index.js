8aab0197217c07d5a24a4cd4cb780ce9bf5bf5d3
Galazan Jonathan (Consultora Ovidio) <ovidio_jgalazan@lanacion.com.ar>
2021-08-13 17:57:43 -0300

    Add Heroku needed config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const allDataAPI = require("./routes/allData");
 const cuentas = require("./routes/cuentas");
 const { logErrors, errorHandler } = require("./utils/middleware/errorHandlers");
@@ -8,6 +8,11 @@ const { logErrors, errorHandler } = require("./utils/middleware/errorHandlers");
 //Routes
 allDataAPI(app);
 cuentas(app);
+
+app.get("/", (request, response) => {
+  response.send("<h1>Examen LN - Backend</h1>");
+});
+
 
 //Middleware
 app.use(logErrors);
